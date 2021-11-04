71c0adeda8f369c9e4dfbcf4f35e47fe2939eb0a
inger_na <inger_na@mail.ru>
2021-11-04 09:47:24 +0100

    Exercises done

--- Exercises/1-pipe.js
+++ Exercises/1-pipe.js
@@ -1,5 +1,10 @@
 'use strict';
 
-const pipe = (...fns) => x => null;
+const pipe = (...args) => {
+  for (const fn of args) {
+    if (typeof fn !== 'function') throw new Error(`${fn} is not a function`);
+  }
+  return x => args.reduce((val, fn) => fn(val), x);
+};
 
 module.exports = { pipe };
